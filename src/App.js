f8a80f990797c146c6f68b06c663b5e036f140ed
James Dunseith <info@workshopmaybe.com>
2020-11-10 10:03:58 -0500

    Align components

--- src/App.js
+++ src/App.js
@@ -15,8 +15,7 @@ import CskDetail from "./components/csks/CskDetail";
 import ApiList from "./components/apis/ApiList";
 // import ApiDetail from "./components/apis/ApiDetail"
 import NotFound from "./components/errors/NotFound";
-// import Playground from "./components/playground/Playground";
-import MessariAxios from "./components/playground/MessariAxios";
+import Playground from "./components/playground/Playground";
 
 function App() {
   return (
@@ -28,7 +27,7 @@ function App() {
         <Route exact path="/cardano-starter-kits" component={CskList} />
         <Route path="/cardano-starter-kits/:id" component={CskDetail} />
         <Route path="/open-source-apis" component={ApiList} />
-        <Route path="/playground" component={MessariAxios} />
+        <Route path="/playground" component={Playground} />
         {/* <Route path="/open-source-apis/:id" component={ApiDetail} /> */}
         <Route component={NotFound} />
       </Switch>
--- src/components/playground/Playground.js
+++ src/components/playground/Playground.js
@@ -1,1 +1,15 @@
+import React, { Component } from 'react'
+import MessariAxios from './MessariAxios';
+
+export class Playground extends Component {
+    render() {
+        return (
+            <div>
+                <MessariAxios />
+            </div>
+        )
+    }
+}
+
+export default Playground
 
